2bc3c36fa22a07c699bcb6c73f3b4194aeff3970
Thanakornkong17 <thanakornkong1710@gmail.com>
2025-02-28 00:26:57 +0700

    Complete 80%

--- data.cpp
+++ data.cpp
@@ -16,5 +16,16 @@ void continueviewRoom(){
     cout << "Continue viewing rooms?    " << endl;
     cout << "1. Yes                     " << endl;
     cout << "2. No                      " << endl;
-                        
+
+    
+    
+}
+void viewQueue(){
+    cout << "==========================" << endl;
+    cout << "Which room will you check?" << endl;
+    cout << "==========================" << endl;
+    cout << "Small Room queue(1)" << endl;
+    cout << "Medium Room queue(2)" << endl;
+    cout << "Large Room queue(3)" << endl;
+    cout << "==========================" << endl;
 }
